15d990fa9517f6b363fc41d0c393f4c3e306dc44
ibrown <ibrown@ebsco.com>
2019-03-27 00:07:53 -0400

    commiting initial code files

---
+++ mlserver.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/v1/ip/<source_ip>/<destination_ip>')
+def information_ip_pair(source_ip, destination_ip):
+
+    return "Currently Under Construction..."
+
+
+@app.route('v1/port/<source_port>/<destination_port>')
+def information_port_pair(source_port, destination_port):
+
+    return "Currently Under Construction..."
